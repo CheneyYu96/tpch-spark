258196bc184144ff8c67b00e4b787cef494fc982
Minchen Yu <myuaj@connect.ust.hk>
2019-01-27 13:28:21 +0800

    add query 31 cross join

---
+++ src/main/scala/Q31.scala
@@ -0,0 +1,25 @@
+package main.scala
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions.avg
+import org.apache.spark.sql.functions.count
+import org.apache.spark.sql.functions.sum
+import org.apache.spark.sql.functions.udf
+/**
+  *
+  */
+class Q31 extends TpchQuery {
+
+  override def execute(sc: SparkContext, schemaProvider: TpchSchemaProvider): DataFrame = {
+
+    // this is used to implicitly convert an RDD to a DataFrame.
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    import sqlContext.implicits._
+    import schemaProvider._
+
+    lineitem.crossJoin(order)
+
+  }
+}
